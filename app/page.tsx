a9299202aeb4d788197034f3367c1b92a21043dc
U097562 <marius.seffer1@saabgroup.com>
2025-01-10 14:47:58 +0100

    renamaed out folder to docs for github pages auto deploy

--- app/page.tsx
+++ app/page.tsx
@@ -3,29 +3,17 @@ import menuData from "../public/menus.json"
 export default function Home() {
   return (
     <div className="p-4">
-      {Object.entries(menuData).map(([restaurantName, menu]) => (
-        <div className="bg-white shadow-md rounded-lg p-6 mb-6" key={restaurantName}>
-          <h1 className="text-2xl font-bold mb-4">{restaurantName}</h1>
-          {Object.entries(menu).map(([sectionName, items]) => (
-            <div className="mb-4" key={sectionName}>
-              {Array.isArray(items) ? (
-                <>
-                  <h2 className="text-xl font-semibold mb-2">{sectionName}</h2>
-                  <ul className="list-disc list-inside">
-                    {items.map((item, index) => (
-                      <li className="text-gray-700 mb-2" key={index}>
-                        {item}
-                      </li>
-                    ))}
-                  </ul>
-                </>
-              ) : (
-                <ul className="list-disc list-inside">
-                  <li className="text-gray-700 mb-2">{items}</li>
-                </ul>
-              )}
-            </div>
-          ))}
+      {menuData.map((restaurant) => (
+        <div className="bg-white shadow-md rounded-lg p-6 mb-6" key={restaurant.name}>
+          <h1 className="text-2xl font-bold mb-2">{restaurant.name}</h1>
+          <p className="text-gray-600 mb-4">{restaurant.location}</p>
+          <ul className="list-disc list-inside">
+            {restaurant.menu.dishes.map((dish, index) => (
+              <li className="text-gray-700 mb-2" key={index}>
+                {dish.name} {dish.price && <span className="text-gray-500">- {dish.price}</span>}
+              </li>
+            ))}
+          </ul>
         </div>
       ))}
     </div>
