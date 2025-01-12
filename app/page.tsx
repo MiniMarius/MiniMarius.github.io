e722799e86e034045b06cc727fa1556ef7fff0b3
MiniMarius <seffermarius@gmail.com>
2025-01-12 21:56:39 +0100

    location data no longer shown

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,30 @@
-import menuData from "../public/menus.json"
+import menuData from "../public/menus.json";
 
 export default function Home() {
   return (
     <div className="p-4">
       {menuData.map((restaurant) => (
-        <div className="bg-white shadow-md rounded-lg p-6 mb-6" key={restaurant.name}>
+        <div
+          className="bg-white shadow-md rounded-lg p-6 mb-6"
+          key={restaurant.name}
+        >
           <h1 className="text-2xl font-bold mb-2">{restaurant.name}</h1>
-          <p className="text-gray-600 mb-4">{restaurant.location}</p>
-          <ul className="list-disc list-inside">
-            {restaurant.menu.dishes.map((dish, index) => (
-              <li className="text-gray-700 mb-2" key={index}>
-                {dish.name} {dish.price && <span className="text-gray-500">- {dish.price}</span>}
-              </li>
-            ))}
-          </ul>
+          {restaurant.menu.dishes.length > 0 ? (
+            <ul className="list-disc list-inside">
+              {restaurant.menu.dishes.map((dish, index) => (
+                <li className="text-gray-700 mb-2" key={index}>
+                  {dish.name}{" "}
+                  {dish.price && (
+                    <span className="text-gray-500">- {dish.price}</span>
+                  )}
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p className="text-gray-500">
+              Inga rätter tillgängliga för tillfället.
+            </p>
+          )}
         </div>
       ))}
     </div>
