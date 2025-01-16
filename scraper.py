b246f60281e0323c75e8fad84b43a68005a62983
U097562 <marius.seffer1@saabgroup.com>
2025-01-16 09:24:13 +0100

    changed headers to more basic

--- scraper.py
+++ scraper.py
@@ -54,16 +54,7 @@ def clean_menus(menus):
 menus = []
 
 # Garros
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-    'Accept-Language': 'en-US,en;q=0.9',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Connection': 'keep-alive',
-    'Upgrade-Insecure-Requests': '1',
-    'DNT': '1',
-    'Cache-Control': 'max-age=0'
-}
+headers = {'User-Agent': 'Mozilla/5.0'}
 try:
     garroshtml = requests.get("https://bistrogarros.se/menyer/meny", headers=headers)
     garroshtml.raise_for_status()
