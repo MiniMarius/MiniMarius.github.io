37c0e590bcb83337080e201a70791bc5e00f9bc2
U097562 <marius.seffer1@saabgroup.com>
2025-01-16 09:20:41 +0100

    added headers to al caminetto http request

--- scraper.py
+++ scraper.py
@@ -54,7 +54,16 @@ def clean_menus(menus):
 menus = []
 
 # Garros
-headers = {'User-Agent': 'Mozilla/5.0'}
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'DNT': '1',
+    'Cache-Control': 'max-age=0'
+}
 try:
     garroshtml = requests.get("https://bistrogarros.se/menyer/meny", headers=headers)
     garroshtml.raise_for_status()
@@ -117,7 +126,7 @@ menus.append({
 
 # Al Caminetto
 try:
-    alCamiHtml = requests.get("https://www.kvartersmenyn.se/index.php/rest/14320")
+    alCamiHtml = requests.get("https://www.kvartersmenyn.se/index.php/rest/14320", headers=headers)
     alCamiHtml.raise_for_status()
     alCamiSoup = BeautifulSoup(alCamiHtml.content, 'html.parser')
 
